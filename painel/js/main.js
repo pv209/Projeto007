4fe7a799e2f3fa9495251ce97522a53613b40f47
Paulo <pvtrybe209@gmail.com>
2023-08-18 11:07:57 -0300

    adicionando menu dinamico

--- painel/js/main.js
+++ painel/js/main.js
@@ -1,17 +1,32 @@
 $(function(){
    var open = true;
    var windowSize = $(window)[0].innerWidth;
+   var  targetSizeMenu = (windowSize <= 400) ? 200 : 300 ;
+
    if(windowSize <= 768){
+      $('.menu').css('width','0');
+      $('.menu').css('padding','0');
       open = false;    
    }
 
    $('.menu-btn').click(function(){
         if(open){
-            $('.menu').animate({'width': '0','padding':'0' });
-            $('header').animate({'left': '0' });
-            $('header').css({'width':'100%' });
-            $('.content').animate({'left': '0' });
-            $('.content').css({'width':'100%' });
-        }
-   })
+            $('.menu').animate({'width': '0','padding':'0' },function(){
+               open = false;
+            });
+            $('.content,header').css('width','100%');
+            $('.content,header').animate({ 'left': '0' },function(){
+               open =  false;
+            });
+        }else{
+            $('.menu').css('display', 'block');
+            $('.menu').animate({'width': targetSizeMenu+'px','padding':'10px' },function(){
+               open = true;
+            });
+            // $('.content,header').css('width','calc(100% - 300px)');
+            $('.content,header').animate({ 'left': targetSizeMenu+'px' },function(){
+               open =  true;
+        })
+   }
 })
+});
